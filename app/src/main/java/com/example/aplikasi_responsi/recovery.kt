bf19efc56c1fe29eb608b5818f312bbec495e267
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-01-10 14:40:24 +0700

    keren

---
+++ app/src/main/java/com/example/aplikasi_responsi/recovery.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasi_responsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recovery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
